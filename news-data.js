2eb3a6fb9a32214dafd2bae815615760f579c1d2
Ui Sasaki <uiuisasaki@gmail.com>
2026-09-23 21:05:35 +0900

    peer-link

--- news-data.js
+++ news-data.js
@@ -2,7 +2,7 @@ const newsData = [
   {
     id: "website-launch",
     date: "2026.09.07",
-    title: "White Coat Society Japan 公式ウェブサイトを公開しました",
+    title: "White Coat Society Japan 公式ウェブサイト公開",
     content: `
       <p>
         この度、White Coat Society Japanの公式ウェブサイトを公開しました。
@@ -25,5 +25,34 @@ const newsData = [
         今後ともWhite Coat Society Japanをよろしくお願いいたします。
       </p>
     `
-  }
+  },
+{
+  id: "peer-link",
+  date: "2026.09.13",
+  title: "一般社団法人ピアリング様とのオンライン講演会を開催",
+  content: `
+    <p>
+      9月12日に一般社団法人ピアリング様と「がんになって変わった日常」という講演会をオンラインで開催させて頂きました。
+      普段あまり触れることのない、「医療を受ける側」の視点を学ぶ、とても貴重な機会となりました。
+
+      ピアリング様、お忙しい中本当にありがとうございました！
+    </p>
+
+    <img
+      src="images/peer-link.png"
+      alt="一般社団法人ピアリング様とのオンライン講演会"
+      class="news-image"
+    >
+
+    <a
+      href="https://www.instagram.com/p/Dddy0e5INn8/"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="instagram-post-link"
+    >
+      <i class="fa-brands fa-instagram"></i>
+      Instagramの投稿を見る
+    </a>
+  `
+}
 ];
